cc4fdcda2051d499956827cfc466aa2c46a72e6e
rsy <626929968@qq.com>
2016-03-08 01:43:59 +0800

    package

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -10,9 +10,6 @@ var users = require('./routes/users');
 
 
 var settings = require('./settings');
-
-var markdown = require('markdown-js');
-var multer = require('multer');
 
 var ejs = require('ejs');
 
@@ -28,7 +25,7 @@ app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
